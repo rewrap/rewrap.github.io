261d1cb57fc500aa3ed6a2ad18d29af78873a8a5
Android-Penguin <mail@shauncb.xyz>
2021-04-26 21:31:28 +1200

    Made email button better
    - Email button now copies email address to clipboard then offers user to open email service or return to website.
    
    - Added some annoying hidden stuff (text input field with email address) because copying predetermined text is way harder than it should be. >:(
    
    - Created global functions JS file for functions that every page needs (Such as email copying script). This optimises website as the code is only written once.
    
    - Shortened footer cos it was too tall.

---
+++ global_functions.js
@@ -0,0 +1,13 @@
+function emailPrompt() {
+    var copyText = document.getElementById("hiddenEmail");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    var openEmail = window.confirm("'rewrapvinyl@gmail.com' copied to clipboard. Press ok to open email provider or cancel to return to website");
+    if(openEmail == true) {
+        window.open("mailto:rewrapvinyl@gmail.com", "_blank", "noopener");
+    } else {
+        //don't do anything lol
+    }
+
+}
